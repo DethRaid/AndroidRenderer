ab34e62961431cf60414ee518259375a3d3fa8a0
David Dubois <davidduboisengineer@gmail.com>
2024-11-17 15:26:33 -0800

    Moved mesh_voxelizer to new API

--- RenderCore/model_import/mesh_voxelizer.cpp
+++ RenderCore/model_import/mesh_voxelizer.cpp
@@ -48,67 +48,47 @@ VoxelTextures MeshVoxelizer::voxelize_with_raster(
     auto& allocator = backend->get_global_allocator();
 
     const auto voxels = allocator.create_volume_texture(
-        "Mesh voxel buffer", VK_FORMAT_R8G8B8A8_UNORM, voxel_texture_resolution, 1, TextureUsage::StorageImage
+        "Mesh voxel buffer",
+        VK_FORMAT_R8G8B8A8_UNORM,
+        voxel_texture_resolution,
+        1,
+        TextureUsage::StorageImage
     );
 
     const auto frustums_buffer = allocator.create_buffer(
-        "Voxelizer frustums", sizeof(glm::mat4), BufferUsage::StagingBuffer
+        "Voxelizer frustums",
+        sizeof(glm::mat4),
+        BufferUsage::StagingBuffer
     );
     auto* bounds_frustum_matrix = allocator.map_buffer<glm::mat4>(frustums_buffer);
     *bounds_frustum_matrix = glm::ortho(
-        bounds.min.x, bounds.max.x, bounds.max.y, bounds.min.y, bounds.max.z, bounds.min.z
+        bounds.min.x,
+        bounds.max.x,
+        bounds.max.y,
+        bounds.min.y,
+        bounds.max.z,
+        bounds.min.z
     );
 
-    graph.begin_render_pass(
-        {
-            .name = "Vozelization",
-            .textures = {
-                {
-                    voxels,
-                    {
-                        .stage = VK_PIPELINE_STAGE_2_FRAGMENT_SHADER_BIT,
-                        .access = VK_ACCESS_2_SHADER_STORAGE_WRITE_BIT,
-                        .layout = VK_IMAGE_LAYOUT_GENERAL
-                    }
-                }
-            },
-            .buffers = {
-                {
-                    primitive_buffer,
-                    {.stage = VK_SHADER_STAGE_FRAGMENT_BIT, .access = VK_ACCESS_2_SHADER_STORAGE_WRITE_BIT}
-                },
-                {frustums_buffer, {.stage = VK_SHADER_STAGE_VERTEX_BIT, .access = VK_ACCESS_2_UNIFORM_READ_BIT}}
-            }
-        }
-    );
-
-    graph.add_subpass(
-        {
+    auto set = DescriptorSet{
+                   *backend,
+                backend->get_transient_descriptor_allocator(),
+                   voxelization_pipeline->get_descriptor_set_info(0)
+               }
+               .bind(0, voxels)
+               .bind(1, primitive_buffer)
+               .bind(2, frustums_buffer)
+               .finalize();
+    graph.add_render_pass(
+        DynamicRenderingPass{
             .name = "Voxelization",
-            .execute = [=, this, &mesh_storage](CommandBuffer& commands) {
-                const auto set = *vkutil::DescriptorBuilder::begin(
-                                      *backend, backend->get_transient_descriptor_allocator()
-                                  )
-                                  .bind_image(
-                                      0, {.image = voxels, .image_layout = VK_IMAGE_LAYOUT_GENERAL},
-                                      VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
-                                      VK_SHADER_STAGE_FRAGMENT_BIT
-                                  )
-                                  .bind_buffer(
-                                      1, {.buffer = primitive_buffer}, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
-                                      VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT
-                                  )
-                                  .bind_buffer(
-                                      2, {.buffer = frustums_buffer}, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
-                                      VK_SHADER_STAGE_VERTEX_BIT
-                                  )
-                                  .build();
-
+            .descriptor_sets = {set},
+            .execute = [this, &mesh_storage, &primitive, set = std::move(set)](CommandBuffer& commands) {
                 commands.bind_vertex_buffer(0, mesh_storage.get_vertex_position_buffer());
                 commands.bind_vertex_buffer(1, mesh_storage.get_vertex_data_buffer());
                 commands.bind_index_buffer(mesh_storage.get_index_buffer());
 
-                commands.bind_descriptor_set(0, set);
+                commands.bind_descriptor_set(0, set.get_vk_descriptor_set());
                 commands.bind_descriptor_set(1, backend->get_texture_descriptor_pool().get_descriptor_set());
 
                 commands.set_push_constant(0, primitive.index);
@@ -117,14 +97,14 @@ VoxelTextures MeshVoxelizer::voxelize_with_raster(
 
                 const auto& mesh = primitive->mesh;
                 commands.draw_indexed(
-                    mesh->num_indices, 1, static_cast<uint32_t>(mesh->first_index),
-                    static_cast<uint32_t>(mesh->first_vertex), 0
+                    mesh->num_indices,
+                    1,
+                    static_cast<uint32_t>(mesh->first_index),
+                    static_cast<uint32_t>(mesh->first_vertex),
+                    0
                 );
             }
-        }
-    );
-
-    graph.end_render_pass();
+        });
 
     return {.num_voxels = voxel_texture_resolution, .color_texture = voxels};
 }
@@ -144,11 +124,19 @@ VoxelTextures MeshVoxelizer::voxelize_with_compute(
     auto& allocator = backend->get_global_allocator();
 
     const auto voxels_color = allocator.create_volume_texture(
-        "Mesh voxel colors", VK_FORMAT_R8G8B8A8_UNORM, voxel_texture_resolution, 1, TextureUsage::StorageImage
+        "Mesh voxel colors",
+        VK_FORMAT_R8G8B8A8_UNORM,
+        voxel_texture_resolution,
+        1,
+        TextureUsage::StorageImage
     );
 
     const auto voxels_normal = allocator.create_volume_texture(
-        "Mesh voxel normals", VK_FORMAT_R16G16B16A16_SNORM, voxel_texture_resolution, 1, TextureUsage::StorageImage
+        "Mesh voxel normals",
+        VK_FORMAT_R16G16B16A16_SNORM,
+        voxel_texture_resolution,
+        1,
+        TextureUsage::StorageImage
     );
 
     const auto pass_parameters = VoxelizerComputePassParameters{
@@ -185,7 +173,7 @@ VoxelTextures MeshVoxelizer::voxelize_primitive(
     RenderGraph& graph, const MeshPrimitiveHandle primitive, const MeshStorage& mesh_storage,
     const BufferHandle primitive_buffer, const float voxel_size, const Mode mode
 ) const {
-    switch (cvar_voxelization_method.Get()) {
+    switch(cvar_voxelization_method.Get()) {
     case VoxelizationMethod::RasterPipeline:
         return voxelize_with_raster(graph, primitive, mesh_storage, primitive_buffer, voxel_size);
 
