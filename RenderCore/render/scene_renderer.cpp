4e29c4e7c069d0ae44dca1d7c8f710187b0d33ba
David Dubois <davidduboisengineer@gmail.com>
2024-12-24 09:55:26 -0800

    Turn VRS on by default

--- RenderCore/render/scene_renderer.cpp
+++ RenderCore/render/scene_renderer.cpp
@@ -31,7 +31,7 @@ static auto cvar_use_lpv = AutoCVar_Int{
 };
 
 static auto cvar_anti_aliasing = AutoCVar_Enum{
-    "r.AntiAliasing", "What kind of antialiasing to use", AntiAliasingType::None
+    "r.AntiAliasing", "What kind of antialiasing to use", AntiAliasingType::VRSAA
 };
 // ReSharper restore CppDeclaratorNeverUsed
 
